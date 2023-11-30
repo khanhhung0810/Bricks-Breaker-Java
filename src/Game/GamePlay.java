e89ad307a759fff9b0a48d467e93be8768161fd8
khanhhung0810 <lhkhungg@gmail.com>
2023-11-30 16:43:09 +0700

    new

--- src/Game/GamePlay.java
+++ src/Game/GamePlay.java
@@ -7,27 +7,27 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class GamePlay  extends JPanel implements KeyListener, ActionListener {
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private boolean play = true;
     private int score = 0;
-
+    private int currentLevel = 1;
     private int totalBricks = 21;
 
     private Timer timer;
-    private int delay = 8;
+    private int delay = 5;
 
     private int playerX = 310;
 
     private int ballposX = 120;
     private int ballposY = 350;
-    private int ballXdir = -1;
-    private int ballYdir = -2;
-
+    private int ballXdir = -2;
+    private int ballYdir = -4;
     private MapGenerator map;
 
 
     public GamePlay() {
         map = new MapGenerator(3, 7);
+        map.setLevel(currentLevel);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -38,7 +38,7 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
     public void paint(Graphics g) {
 
         //background color
-        g.setColor(new Color(0x06CFEC));
+        g.setColor(Color.YELLOW);
         g.fillRect(1, 1, 692, 592);
 
         map.draw((Graphics2D)g);
@@ -48,41 +48,64 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
         g.fillRect(691, 0, 3, 592);
 
         g.setColor(Color.blue);
-        g.fillRect(playerX, 550, 100, 12);
+        g.fillRect(playerX, 550, 200, 12);
 
-        g.setColor(Color.BLACK);  // ball color
+        g.setColor(Color.RED);  // ball color
         g.fillOval(ballposX, ballposY, 20, 20);
 
         g.setColor(Color.black);
-        g.setFont(new Font("MV Boli", Font.BOLD, 25));
+        g.setFont(new Font("MV Arial", Font.BOLD, 25));
         g.drawString("Score: " + score, 520, 30);
 
 
-        if (totalBricks <= 0) { // if all bricks are destroyed then you win
-            play = false;
-            ballXdir = 0;
-            ballYdir = 0;
-            g.setColor(new Color(0x0D02FF));
-            g.setFont(new Font("MV Boli", Font.BOLD, 30));
-            g.drawString("You Won, Score: " + score, 190, 300);
+//        if (totalBricks <= 0) { // if all bricks are destroyed then you win
+//            play = false;
+//            ballXdir = 0;
+//            ballYdir = 0;
+//            g.setColor(new Color(0XFF6464));
+//            g.setFont(new Font("MV Arial", Font.BOLD, 30));
+//            g.drawString("You Won, Score: " + score, 190, 300);
+//
+//            g.setFont(new Font("MV Arial", Font.BOLD, 20));
+//            g.drawString("Press Enter to Restart.", 230, 350);
+//        }
+        if (totalBricks <= 0) {
+            // Level completed, move to the next level
+            currentLevel++;
+            if (currentLevel < 4) {  // You can add more levels if needed
+                map.setLevel(currentLevel);
+                totalBricks = map.map.length * map.map[0].length;
+                ballposX = 120;
+                ballposY = 350;
+                ballXdir = -2;
+                ballYdir = -4;
+            } else {
+                // All levels completed, you win
+                play = false;
+                ballXdir = 0;
+                ballYdir = 0;
+                g.setColor(new Color(0XFF6464));
+                g.setFont(new Font("MV Arial", Font.BOLD, 30));
+                g.drawString("You Won, Score: " + score, 190, 300);
 
-            g.setFont(new Font("MV Boli", Font.BOLD, 20));
-            g.drawString("Press Enter to Restart.", 230, 350);
+                g.setFont(new Font("MV Arial", Font.BOLD, 20));
+                g.drawString("Press Enter to Restart.", 230, 350);
+            }
         }
-
         if(ballposY > 570) { // if ball goes below the paddle then you lose
             play = false;
             ballXdir = 0;
             ballYdir = 0;
             g.setColor(Color.BLACK);
-            g.setFont(new Font("MV Boli", Font.BOLD, 30));
+            g.setFont(new Font("MV Arial", Font.BOLD, 30));
             g.drawString("Game Over, Score: " + score, 190, 300);
 
-            g.setFont(new Font("MV Boli", Font.BOLD, 20));
+            g.setFont(new Font("MV Arial", Font.BOLD, 20));
             g.drawString("Press Enter to Restart", 230, 350);
 
         }
         g.dispose();
+
     }
 
     @Override
@@ -90,9 +113,11 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
         timer.start();
         if(play) {
             // Ball - Pedal  interaction
-            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
+            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 200, 8))) {
                 ballYdir = - ballYdir;
+
             }
+
             for( int i = 0; i<map.map.length; i++) { // Ball - Brick interaction
                 for(int j = 0; j<map.map[0].length; j++) {  // map.map[0].length is the number of columns
                     if(map.map[i][j] > 0) {
@@ -122,6 +147,7 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
                 }
             }
 
+
             ballposX += ballXdir;
             ballposY += ballYdir;
             if(ballposX < 0) { // if ball hits the left wall then it bounces back
@@ -136,12 +162,8 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
             }
 
         }
-
-
         repaint();
-
     }
-
 
     @Override
     public void keyTyped(KeyEvent arg0) {
@@ -172,8 +194,8 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
                 play = true;
                 ballposX = 120;
                 ballposY = 350;
-                ballXdir = -1;
-                ballYdir = -2;
+                ballXdir = -2;
+                ballYdir = -4;
                 score = 0;
                 totalBricks = 21;
                 map = new MapGenerator(3,7);
@@ -198,4 +220,7 @@ public class GamePlay  extends JPanel implements KeyListener, ActionListener {
     public void keyReleased(KeyEvent arg0) {
 
     }
+
+
 }
+
--- src/Game/MapGenerator.java
+++ src/Game/MapGenerator.java
@@ -19,13 +19,36 @@ public class MapGenerator {
         brickWidth = 540/col;
         brickHeight = 150/row;
     }
+    // new method to set the level
+    public void setLevel(int level) {
+        if (level == 1) {
+            // Level 1 configuration
+            map = new int[][]{{1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1}};
+        } else if (level == 2) {
+            // Level 2 configuration
+            map = new int[][]{{1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1},
+                    {1, 1, 1, 1, 1, 1, 1}};
+        } else if (level == 3) {
+            // Level 3 configuration
+            map = new int[][]{{1, 1, 0, 1, 1, 0, 1},
+                    {1, 1, 0, 1, 1, 0, 1},
+                    {1, 1, 1, 1, 1, 1, 1}};
+        }
+
+        // You can add more levels as needed
+        brickWidth = 540 / map[0].length;
+        brickHeight = 150 / map.length;
+    }
 
     // this draws the bricks
     public void draw(Graphics2D g) {
         for (int i = 0; i < map.length; i++) {
             for (int j=0; j< map[0].length;j++) {
                 if(map[i][j] > 0) {
-                    g.setColor(new Color(0x0D02FF)); // brick color
+                    g.setColor(new Color(0XFF8787)); // brick color
                     g.fillRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
 
                     g.setStroke(new BasicStroke(4));
@@ -41,4 +64,5 @@ public class MapGenerator {
     public void setBrickValue(int value, int row, int col) {
         map[row][col] = value;
     }
+
 }
